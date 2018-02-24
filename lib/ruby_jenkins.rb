783d2f55e10e5e2289df7e6c59bca7a904c4a1e5
Soundar Rathinasamy <soundar.rathinasamy@gmail.com>
2018-02-24 18:40:08 +0530

    Trigger Jenkins build

--- lib/ruby_jenkins.rb
+++ lib/ruby_jenkins.rb
@@ -2,4 +2,8 @@ class RubyJenkins
   def test
     true
   end
+
+  def test1
+    true
+  end
 end
--- spec/ruby_genkins_spec.rb
+++ spec/ruby_genkins_spec.rb
@@ -2,7 +2,15 @@ require 'rspec'
 require File.expand_path('../../lib/ruby_jenkins', __FILE__)
 
 describe RubyJenkins do
-  it 'returns truthy' do
-    expect(RubyJenkins.new.test).to be_truthy
+  describe '#test' do
+    it 'returns truthy' do
+      expect(RubyJenkins.new.test).to be_truthy
+    end
+  end
+
+  describe '#test1' do
+    it 'returns truthy' do
+      expect(RubyJenkins.new.test1).to be_truthy
+    end
   end
 end
